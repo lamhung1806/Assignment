0c6c5ab82a26074c4bc44cba5b7b1a48ffe94f40
ngodatuet <datngo@qsoftvietnam.com>
2022-03-22 13:27:40 +0700

    fix: assignment

--- src/redux/User/UserSelector.ts
+++ src/redux/User/UserSelector.ts
@@ -1,4 +1,4 @@
 import { RootState } from "../reduxStore";
 
-export const sUserList = (state: RootState) => state.User.userList;
+export const sGetUserList = (state: RootState) => state.User.userList;
 export const sGetCurrentPage = (state: RootState) => state.User.currentPage;
